b63365ce98ec0fdfa51bb481ddde5c10a379c3fb
VioletShi-_- <passionatefrisbee@gmail.com>
2023-04-23 14:39:38 +0200

    Apple and sliced apple scripts added

---
+++ VRTest/Assets/FoodPack/Scripts/FruitController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitController : MonoBehaviour
+{
+    BoxCollider boxCollider;
+    Vector3 fruitPos;
+
+    public GameObject slicedFruit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+        fruitPos = transform.position;
+        boxCollider.isTrigger = false;
+        boxCollider.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("sword"))
+        {
+            Instantiate(slicedFruit, fruitPos, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ VRTest/Assets/FoodPack/Scripts/SlicedFruitController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlicedFruitController : MonoBehaviour
+{
+    public float sliceForce = 0.1f;
+    public float fallingTime = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            Rigidbody sliceRb = child.gameObject.GetComponent<Rigidbody>();
+            rgUpdate(sliceRb);
+        }
+        Destroy(gameObject, fallingTime);
+
+    }
+
+    /**
+     * Enable rigidbody and give it gravity/extra force to have the falling effects
+     * @param {rigidbody} the rigidbody to set
+     */
+    private void rgUpdate(Rigidbody rigidbody)
+    {
+        rigidbody.WakeUp();
+        rigidbody.AddForce(Random.insideUnitCircle * sliceForce);
+    }
+}
