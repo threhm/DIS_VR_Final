d8b6069d80e8c7d836e445a2789192d03e79a8f3
threhm <threhm1@gmail.com>
2023-04-23 15:54:13 -0600

    Made changes to the race map

--- VRTest/Assets/Scripts/PlayerMover.cs
+++ VRTest/Assets/Scripts/PlayerMover.cs
@@ -6,9 +6,9 @@ public class PlayerMover : MonoBehaviour
 {
     public GameObject startNode;
 
-    private GameObject currentNode;
+    public GameObject currentNode;
 
-    private GameObject nextNode;
+    public GameObject nextNode;
 
     public float speed;
     
@@ -26,21 +26,22 @@ public class PlayerMover : MonoBehaviour
     void Update()
     {
         //If we are close to the next node then move to another node
-        if((((gameObject.transform.position.x - nextNode.transform.position.x) <= 0.05 ) && ((gameObject.transform.position.x - nextNode.transform.position.x) >= -0.2))
+        if((((gameObject.transform.position.x - nextNode.transform.position.x) <= 0.4 ) && ((gameObject.transform.position.x - nextNode.transform.position.x) >= -0.4))
          &&
-        (((gameObject.transform.position.y - nextNode.transform.position.y) <= 0.05 ) && ((gameObject.transform.position.y - nextNode.transform.position.y) >= -0.2)) 
+        (((gameObject.transform.position.y - nextNode.transform.position.y) <= 0.4 ) && ((gameObject.transform.position.y - nextNode.transform.position.y) >= -0.4)) 
         &&
-        (((gameObject.transform.position.z - nextNode.transform.position.z) <= 0.05 ) && ((gameObject.transform.position.z - nextNode.transform.position.z) >= -0.2))) {
+        (((gameObject.transform.position.z - nextNode.transform.position.z) <= 0.4 ) && ((gameObject.transform.position.z - nextNode.transform.position.z) >= -0.4))) {
             currentNode = nextNode;
             nextNode = currentNode.GetComponent<MovementMarker>().getNextMarker();
-            Debug.Log("Stuck here");
+            speed = currentNode.GetComponent<MovementMarker>().speed;
+            //Debug.Log("Stuck here");
         } else {
             Vector3 difference = nextNode.transform.position - gameObject.transform.position;
             difference.Normalize();
             Vector3 scalar = new Vector3(speed, speed, speed);
             difference.Scale(scalar);
             gameObject.transform.position = gameObject.transform.position + difference;
-            Debug.Log("Moving");
+            //Debug.Log("Moving");
         }
     }
 }
