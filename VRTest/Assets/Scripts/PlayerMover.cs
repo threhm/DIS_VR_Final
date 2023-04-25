a70710be874ba7f86c885b65858e80965112341c
threhm <threhm1@gmail.com>
2023-04-25 07:53:10 -0600

    Made the player turn to face nodes

--- VRTest/Assets/Scripts/PlayerMover.cs
+++ VRTest/Assets/Scripts/PlayerMover.cs
@@ -12,7 +12,7 @@ public class PlayerMover : MonoBehaviour
 
     public float speed;
     
-    
+    public float angleSpeed = 100f;
 
     // Start is called before the first frame update
     
@@ -21,6 +21,13 @@ public class PlayerMover : MonoBehaviour
         currentNode = startNode;
         nextNode = currentNode.GetComponent<MovementMarker>().getNextMarker();
     }
+
+    // To use this script, attach it to the GameObject that you would like to rotate towards another game object.
+// After attaching it, go to the inspector and drag the GameObject you would like to rotate towards into the target field.
+// Move the target around in the scene view to see the GameObject continuously rotate towards it.
+    // The target marker.
+    public Transform target;
+
 
     // Update is called once per frame
     void Update()
@@ -35,6 +42,16 @@ public class PlayerMover : MonoBehaviour
             nextNode = currentNode.GetComponent<MovementMarker>().getNextMarker();
             speed = currentNode.GetComponent<MovementMarker>().speed;
             //Debug.Log("Stuck here");
+
+            //rotate towards the new target
+            Vector3 targetDirection = nextNode.transform.position - this.transform.position;
+            float singleStep = speed * Time.deltaTime;
+
+            // Rotate the forward vector towards the target direction by one step
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, angleSpeed, 0.0f);
+            // Calculate a rotation a step closer to the target and applies rotation to this object
+            this.transform.rotation = Quaternion.LookRotation(newDirection);
+
         } else {
             Vector3 difference = nextNode.transform.position - gameObject.transform.position;
             difference.Normalize();
