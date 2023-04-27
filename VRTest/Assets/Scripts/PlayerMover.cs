43d4c7fdeaebf67145a19b812855fab66dabd735
threhm <threhm1@gmail.com>
2023-04-27 08:19:42 -0600

    Added turning, a broom, and magic sparkles

--- VRTest/Assets/Scripts/PlayerMover.cs
+++ VRTest/Assets/Scripts/PlayerMover.cs
@@ -11,8 +11,21 @@ public class PlayerMover : MonoBehaviour
     public GameObject nextNode;
 
     public float speed;
+
+    //Can be changed to make the player turn faster or slower
+    public float turnMagnitude = 10f;
     
-    public float angleSpeed = 100f;
+    //public float angleSpeed;
+
+    //private Vector3 targetDirection;
+   // private float singleStep;
+    //private Vector3 newDirection;
+
+    //public float turnTimer;
+    //private float timer;
+    //private bool turning;
+
+    //private Quaternion neededRotation;
 
     // Start is called before the first frame update
     
@@ -20,11 +33,13 @@ public class PlayerMover : MonoBehaviour
     {
         currentNode = startNode;
         nextNode = currentNode.GetComponent<MovementMarker>().getNextMarker();
+        //turning = false;
+        //timer = turnTimer;
     }
 
     // To use this script, attach it to the GameObject that you would like to rotate towards another game object.
-// After attaching it, go to the inspector and drag the GameObject you would like to rotate towards into the target field.
-// Move the target around in the scene view to see the GameObject continuously rotate towards it.
+    // After attaching it, go to the inspector and drag the GameObject you would like to rotate towards into the target field.
+    // Move the target around in the scene view to see the GameObject continuously rotate towards it.
     // The target marker.
     public Transform target;
 
@@ -32,7 +47,12 @@ public class PlayerMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //If we are close to the next node then move to another node
+
+        //Turn the player towards the next node
+        Quaternion neededRotation = Quaternion.LookRotation((nextNode.transform.position - this.transform.position));    
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, neededRotation, Time.deltaTime * turnMagnitude);   
+
+        //If we are close to the next node then move to another node otherwise keep moving the player towards the node that they're already going towards
         if((((gameObject.transform.position.x - nextNode.transform.position.x) <= 0.4 ) && ((gameObject.transform.position.x - nextNode.transform.position.x) >= -0.4))
          &&
         (((gameObject.transform.position.y - nextNode.transform.position.y) <= 0.4 ) && ((gameObject.transform.position.y - nextNode.transform.position.y) >= -0.4)) 
@@ -41,17 +61,6 @@ public class PlayerMover : MonoBehaviour
             currentNode = nextNode;
             nextNode = currentNode.GetComponent<MovementMarker>().getNextMarker();
             speed = currentNode.GetComponent<MovementMarker>().speed;
-            //Debug.Log("Stuck here");
-
-            //rotate towards the new target
-            Vector3 targetDirection = nextNode.transform.position - this.transform.position;
-            float singleStep = speed * Time.deltaTime;
-
-            // Rotate the forward vector towards the target direction by one step
-            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, angleSpeed, 0.0f);
-            // Calculate a rotation a step closer to the target and applies rotation to this object
-            this.transform.rotation = Quaternion.LookRotation(newDirection);
-
         } else {
             Vector3 difference = nextNode.transform.position - gameObject.transform.position;
             difference.Normalize();
